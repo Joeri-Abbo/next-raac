270d50c0991d7cf517b05001fb90fff4cf0627c4
Joeri Abbo <joeriabbo@hotmail.com>
2022-12-28 21:18:58 +0100

    Add contact form functionality

---
+++ components/ContactForm.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+const Logo = () => {
+    return (
+        <Link href="/" className="my-2 flex items-center space-x-1 text-blue-500">
+            <span className="font-bold text-3xl font-sans tracking-tight whitespace-nowrap">
+                Richard Abbo
+            </span>
+        </Link>
+    )
+}
+
+export default Logo;

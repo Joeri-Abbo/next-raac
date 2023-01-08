be6c61029b0ece982ddcf8f8541af88281905ce6
Joeri Abbo <joeriabbo@hotmail.com>
2023-01-08 11:29:33 +0100

    Fix issue

--- components/Sections/Footer/ToTop.tsx
+++ components/Sections/Footer/ToTop.tsx
@@ -3,7 +3,7 @@ import ArrowUp from "../../../public/icons/circle-arrow-up-solid.svg";
 
 const ToTop = () => {
     return (
-        <Link href="/#top" className="absolute left-3">
+        <Link href="#top" className="absolute left-3">
             <ArrowUp className="w-8 h-8 fill-blue-500 dark:fill-white" role="button"/>
         </Link>
     )

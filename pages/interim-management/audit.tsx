5b6613aba704d11646f806aee4db61adef0adc1c
Joeri Abbo <joeriabbo@hotmail.com>
2023-01-09 21:17:06 +0100

    Wip new pages

---
+++ pages/interim-management/audit.tsx
@@ -0,0 +1,33 @@
+import Layout from "../../sections/Layout";
+import Head from "../../sections/Head";
+import {useTranslation} from "react-i18next";
+import Panorama from "../../components/Panorama";
+import Main from "../../sections/Main";
+import Col2 from "../../components/Col2";
+import BoxContent from "../../components/BoxContent";
+import BoxImage from "../../components/BoxImage";
+
+export default function Audit() {
+    const {t} = useTranslation('interimmanagement')
+
+    return (
+        <>
+            <Layout>
+
+                <Head>
+                    <title> {t('seo.title')}</title>
+                    <meta name="description"
+                          content={t('seo.description') ?? ""}/>
+                </Head>
+                <Panorama image="/panorama/header-image-9.jpg" alt="panorama" title={t('title') ?? ""}/>
+                <Main>
+                    <Col2>
+                        <BoxContent title={t('box1.title') ?? ""} text={t('box1.text') ?? ""}/>
+                        <BoxImage src={"/content/management.png"} alt={"TEST"} width={1760} height={1760}/>
+                        <BoxContent title={t('box2.title') ?? ""} text={t('box2.text') ?? ""}/>
+                    </Col2>
+                </Main>
+            </Layout>
+        </>
+    )
+}
--- pages/interim-management/index.tsx
+++ pages/interim-management/index.tsx
@@ -1,19 +1,18 @@
-import Layout from "../sections/Layout";
-import Head from "../sections/Head";
+import Layout from "../../sections/Layout";
+import Head from "../../sections/Head";
 import {useTranslation} from "react-i18next";
-import Panorama from "../components/Panorama";
-import Main from "../sections/Main";
-import Col2 from "../components/Col2";
-import BoxContent from "../components/BoxContent";
-import BoxImage from "../components/BoxImage";
+import Panorama from "../../components/Panorama";
+import Main from "../../sections/Main";
+import Col2 from "../../components/Col2";
+import BoxContent from "../../components/BoxContent";
+import BoxImage from "../../components/BoxImage";
 
-export default function Customers() {
+export default function Index() {
     const {t} = useTranslation('interimmanagement')
 
     return (
         <>
             <Layout>
-
                 <Head>
                     <title> {t('seo.title')}</title>
                     <meta name="description"

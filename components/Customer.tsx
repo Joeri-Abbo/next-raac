56aa2024f2591f64b7ecc51129cacd77d6211676
Joeri Abbo <joeriabbo@hotmail.com>
2023-01-14 20:07:11 +0100

    Update logos

--- components/Customer.tsx
+++ components/Customer.tsx
@@ -13,8 +13,10 @@ const Customer = (customer: CustomerType) => {
             <div
                 className=" rounded shadow-lg shadow-gray-200 dark:shadow-gray-900 bg-white dark:bg-gray-800 duration-300 hover:-translate-y-1 h-full">
                 <figure>
-                    <Image src={customer.image} className="rounded-t h-72 w-full object-contain" alt="Me" width="600"
-                           height="600"/>
+                    <div className="p-4">
+                        <Image src={customer.image} className="rounded-t h-72 w-full object-contain" alt="Me" width="600"
+                               height="600"/>
+                    </div>
 
                     <figcaption className="p-4">
                         <p

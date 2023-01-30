ab716b308a8aac1c8d1e67c947e80afb89f120c1
Joeri Abbo <joeriabbo@hotmail.com>
2023-01-30 23:37:41 +0100

    Update map

--- components/Map.tsx
+++ components/Map.tsx
@@ -3,7 +3,7 @@ const Map = () => {
     return (
         <>
             <iframe
-                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2464.85162783622!2d4.1639059!3d51.8454054!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c44e1919eef6ff%3A0xe6a962dd8cee56f8!2sUniceflaan+44%2C+3223+PA+Hellevoetsluis!5e0!3m2!1snl!2snl!4v1389103784629"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2465.2007693785868!2d4.137579415816474!3d51.83902899422211!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c451c36b4ab063%3A0xce223d2d9958d96f!2sMoriaanseweg%20Oost%20219%2C%203223%20AH%20Hellevoetsluis!5e0!3m2!1sen!2snl!4v1675118237539!5m2!1sen!2snl"
                 width="100%" height="450" className="border-0"></iframe>
         </>
     )

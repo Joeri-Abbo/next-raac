01d6c067665b071a50525669afb838a869a3fe7b
Joeri Abbo <joeriabbo@hotmail.com>
2023-03-06 00:14:05 +0100

    Improve seo image

--- sections/Head.tsx
+++ sections/Head.tsx
@@ -1,4 +1,5 @@
 import HeadNext from "next/head";
+import {useRouter} from "next/router";
 
 type Props = {
     title?: string
@@ -6,12 +7,19 @@ type Props = {
 
 }
 const Head = (props: Props) => {
+    const router = useRouter()
+
     return (
         <>
             <HeadNext>
                 <title>{props.title + " - " + "RAAC"}</title>
                 <meta name="description" content={props.description}/>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <meta name="og:title" content={props.title}/>
+                <meta name="og:description" content={props.description}/>
+                <meta name="og:type" content="website"/>
+                <meta name="og:url" content={router.asPath}/>
+                <meta name="og:image" content={"/og-image.jpg"}/>
                 <link rel="icon" href="/favicon.ico"/>
             </HeadNext>
         </>
